b16123d48a00243583481bd2ca0a6aafa1a944b2
Helmy Heiry <nws06_01098906@pb.edu.bn>
2022-12-06 17:52:25 +0800

    edit vite

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import About from './components/About';
 import Work from './components/Work';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import bgImg from '/assets/concrete-wall-texture.svg';
 
 function App() {
   return (
     <div className="">
-      <img className="h-[100%] fixed z-0 object-cover w-full mix-blend-multiply pointer-events-none" src="./assets/concrete-wall-texture.svg" alt="" />
+      <img className="h-[100%] fixed z-0 object-cover w-full mix-blend-multiply pointer-events-none" src={bgImg} alt="" />
       <div className="relative">
       <Header/>
       <Home/>
