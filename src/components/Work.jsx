a0e8166168775e64bd4a0670be93f71f26240c33
Heiry <119796148+HeiryH@users.noreply.github.com>
2022-12-10 11:05:16 +0800

    change colors

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,7 +7,7 @@ const work = () => {
     <div>
       <div className="h-[100vh] flex flex-col justify-center break-words">
         {Works.map(project => (
-          <a className="lg:w-fit hover:text-[#D6D5A8] grow-[0.1] lg:grow-0" href={project.link}>
+          <a className="lg:w-fit grow-[0.1] lg:grow-0" href={project.link}>
           <WorkLinks  title={project.title}/>
           </a>
         ))}
